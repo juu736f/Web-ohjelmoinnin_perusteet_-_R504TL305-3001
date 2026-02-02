84fd5baedcf6abcbe349943033f8f0fc3115125b
Juuso Kekonen <juuso@kekonen.com>
2026-02-02 09:29:27 +0200

    pusketaan tehtävä 9

--- 3-harjoitus/exercise_9/js/main.js
+++ 3-harjoitus/exercise_9/js/main.js
@@ -1,10 +1,14 @@
 const url = "https://jsonplaceholder.typicode.com/todos/"
+let todos = []
 
 document.addEventListener("DOMContentLoaded", (event) => {
-
+    resetUserInputs()
 });
 
-function loadTodosToTable(todos) {
+function generateTableForTodos(todos) {
+    if (!Array.isArray(todos) || todos.length === 0) {
+        return '<p>No results</p>';
+    }
     const keys = Object.keys(todos[0]);
     
     let html = '<table><tr>'; 
@@ -17,7 +21,7 @@ function loadTodosToTable(todos) {
         html += '</tr>';
     });
     html += '</table>';
-    document.getElementById("todostable").innerHTML = html;
+    return html;
 }
 
 async function getData() {
@@ -30,12 +34,67 @@ async function getData() {
     }
 }
 
+function generateOptions(arr) {
+    let html = `<option value="" selected disabled hidden>Choose here</option>`;
+    let userIds = []
+    for (const item of arr) {
+        userIds.push(item.userId)
+    }
+    unique_userIds = [...new Set(userIds)]    
+    for (ids in unique_userIds) {
+        html += `<option name="${unique_userIds[ids]}" id=${unique_userIds[ids]}>${unique_userIds[ids]}</option>`
+    }
+    html += `<option name="all" id="all">all</option>`
+    document.getElementById("userselect").innerHTML = html
+}
+
+function resetUserInputs() {
+    document.getElementById("titlequery").value = ""
+    document.getElementById("completedcheck").checked = false
+    document.getElementById("userselect").value = ""
+}
+
+function getUserInput() {
+    let query = document.getElementById("titlequery").value
+    let completed = document.getElementById("completedcheck").checked
+    let uid = document.getElementById("userselect").value
+    return [query, completed, uid]
+}
+
+function updateTable() {
+    let result = todos
+    const arr = getUserInput()
+    const query = arr[0]      
+    const completed = arr[1]  // ok
+    const uid = arr[2]        // ok
+
+    if (completed !== false) {
+        result = result.filter(p => p.completed === completed);
+    }
+    
+    if (uid !== null && uid !== "" && uid !== "all") {
+        result = result.filter(p => p.userId === Number(uid))
+    }
+    
+    if (uid === "all") {
+        result = result.filter(p => p.userId !== null)
+    }
+
+    if (query !== "" && query !== null) {
+        const q = query.toLowerCase();
+        result = result.filter(p => String(p.title).toLowerCase().includes(q));
+    }
+    document.getElementById("todostable").innerHTML = generateTableForTodos(result)
+}
+
 getData().then(result => {
-    loadTodosToTable(result)
+    console.log(result)
+    todos = result
+    generateOptions(result)
+    document.getElementById("todostable").innerHTML = generateTableForTodos(result)
 })
 
 
+document.addEventListener("DOMContentLoaded", () => {
+});
 
-/*
-fetch('https://jsonplaceholder.typicode.com/todos/1').then(response => response.json()).then(json => data = json)
-*/
